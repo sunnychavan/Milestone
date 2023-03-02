13de783919f2d0b2df640166309a37c661662f0b
Connor McCarthy <connor15mcc@gmail.com>
2023-03-01 23:07:01 -0500

    introduce static lookups to heuristics

--- src/ai/heuristics.rs
+++ src/ai/heuristics.rs
@@ -8,20 +8,11 @@ use std::{
     ops::Div,
 };
 
+use lazy_static::lazy_static;
+
 use crate::game::{
     board::Hole, gamestate::State, pieces::Piece::Black, pieces::Piece::White,
 };
-
-fn number_of_pieces(state: &State) -> i64 {
-    let raw_score = state
-        .board
-        .current_players_pieces(0)
-        .len()
-        .try_into()
-        .unwrap_or(0);
-
-    unsigned100_normalize(0, 10, raw_score)
-}
 
 fn piece_differential(state: &State) -> i64 {
     let current_player_num = state
@@ -41,86 +32,96 @@ fn piece_differential(state: &State) -> i64 {
 }
 
 fn hold_important_pieces(state: &State) -> i64 {
-    let mut important_pieces: HashMap<usize, i64> = HashMap::new();
-    important_pieces.insert(0, 3);
-    important_pieces.insert(1, 1);
-    important_pieces.insert(2, 1);
-    important_pieces.insert(36, 3);
-    important_pieces.insert(34, 1);
-    important_pieces.insert(35, 1);
+    lazy_static! {
+        static ref IMPORTANT_PIECES: HashMap<usize, i64> = {
+            let mut hm = HashMap::new();
+            hm.insert(0, 3);
+            hm.insert(1, 1);
+            hm.insert(2, 1);
+            hm.insert(36, 3);
+            hm.insert(34, 1);
+            hm.insert(35, 1);
+            hm
+        };
+    }
 
     let black_score: i64 = state
         .board
         .current_players_pieces(0)
         .iter()
-        .map(|&elt| important_pieces.get(&elt).unwrap_or(&0))
+        .map(|&elt| IMPORTANT_PIECES.get(&elt).unwrap_or(&0))
         .sum();
 
     let white_score: i64 = state
         .board
         .current_players_pieces(1)
         .iter()
-        .map(|&elt| important_pieces.get(&elt).unwrap_or(&0))
+        .map(|&elt| IMPORTANT_PIECES.get(&elt).unwrap_or(&0))
         .sum();
 
     unsigned100_normalize(-5, 5, black_score - white_score)
 }
 
 fn middle_proximity(state: &State) -> i64 {
-    let mut middle_proximity: HashMap<usize, i64> = HashMap::new();
-    middle_proximity.insert(0, 6);
-    middle_proximity.insert(4, 6);
-    middle_proximity.insert(11, 6);
-    middle_proximity.insert(18, 6);
-    middle_proximity.insert(25, 6);
-    middle_proximity.insert(32, 6);
-    middle_proximity.insert(36, 6);
+    lazy_static! {
+        static ref MIDDLE_PROXIMITY: HashMap<usize, i64> = {
+            let mut middle_proximity: HashMap<usize, i64> = HashMap::new();
+            middle_proximity.insert(0, 6);
+            middle_proximity.insert(4, 6);
+            middle_proximity.insert(11, 6);
+            middle_proximity.insert(18, 6);
+            middle_proximity.insert(25, 6);
+            middle_proximity.insert(32, 6);
+            middle_proximity.insert(36, 6);
 
-    middle_proximity.insert(1, 3);
-    middle_proximity.insert(2, 3);
-    middle_proximity.insert(7, 3);
-    middle_proximity.insert(8, 3);
-    middle_proximity.insert(14, 3);
-    middle_proximity.insert(15, 3);
-    middle_proximity.insert(21, 3);
-    middle_proximity.insert(22, 3);
-    middle_proximity.insert(28, 3);
-    middle_proximity.insert(29, 3);
-    middle_proximity.insert(34, 3);
-    middle_proximity.insert(35, 3);
+            middle_proximity.insert(1, 3);
+            middle_proximity.insert(2, 3);
+            middle_proximity.insert(7, 3);
+            middle_proximity.insert(8, 3);
+            middle_proximity.insert(14, 3);
+            middle_proximity.insert(15, 3);
+            middle_proximity.insert(21, 3);
+            middle_proximity.insert(22, 3);
+            middle_proximity.insert(28, 3);
+            middle_proximity.insert(29, 3);
+            middle_proximity.insert(34, 3);
+            middle_proximity.insert(35, 3);
 
-    middle_proximity.insert(3, 1);
-    middle_proximity.insert(5, 1);
-    middle_proximity.insert(10, 1);
-    middle_proximity.insert(12, 1);
-    middle_proximity.insert(17, 1);
-    middle_proximity.insert(19, 1);
-    middle_proximity.insert(24, 1);
-    middle_proximity.insert(26, 1);
-    middle_proximity.insert(31, 1);
-    middle_proximity.insert(33, 1);
+            middle_proximity.insert(3, 1);
+            middle_proximity.insert(5, 1);
+            middle_proximity.insert(10, 1);
+            middle_proximity.insert(12, 1);
+            middle_proximity.insert(17, 1);
+            middle_proximity.insert(19, 1);
+            middle_proximity.insert(24, 1);
+            middle_proximity.insert(26, 1);
+            middle_proximity.insert(31, 1);
+            middle_proximity.insert(33, 1);
 
-    middle_proximity.insert(6, 0);
-    middle_proximity.insert(9, 0);
-    middle_proximity.insert(13, 0);
-    middle_proximity.insert(16, 0);
-    middle_proximity.insert(20, 0);
-    middle_proximity.insert(23, 0);
-    middle_proximity.insert(27, 0);
-    middle_proximity.insert(30, 0);
+            middle_proximity.insert(6, 0);
+            middle_proximity.insert(9, 0);
+            middle_proximity.insert(13, 0);
+            middle_proximity.insert(16, 0);
+            middle_proximity.insert(20, 0);
+            middle_proximity.insert(23, 0);
+            middle_proximity.insert(27, 0);
+            middle_proximity.insert(30, 0);
 
+            middle_proximity
+        };
+    }
     let black_score: i64 = state
         .board
         .current_players_pieces(0)
         .iter()
-        .map(|&elt| middle_proximity.get(&elt).unwrap_or(&0))
+        .map(|&elt| MIDDLE_PROXIMITY.get(&elt).unwrap_or(&0))
         .sum();
 
     let white_score: i64 = state
         .board
         .current_players_pieces(1)
         .iter()
-        .map(|&elt| middle_proximity.get(&elt).unwrap_or(&0))
+        .map(|&elt| MIDDLE_PROXIMITY.get(&elt).unwrap_or(&0))
         .sum();
 
     unsigned100_normalize(
@@ -131,27 +132,33 @@ fn middle_proximity(state: &State) -> i64 {
 }
 
 fn middle_piece_differential(state: &State) -> i64 {
-    let mut middle_pieces = HashSet::new();
-    middle_pieces.insert(0);
-    middle_pieces.insert(4);
-    middle_pieces.insert(11);
-    middle_pieces.insert(18);
-    middle_pieces.insert(25);
-    middle_pieces.insert(32);
-    middle_pieces.insert(36);
+    lazy_static! {
+        static ref MIDDLE_PIECES: HashSet<usize> = {
+            let mut middle_pieces = HashSet::new();
+            middle_pieces.insert(0);
+            middle_pieces.insert(4);
+            middle_pieces.insert(11);
+            middle_pieces.insert(18);
+            middle_pieces.insert(25);
+            middle_pieces.insert(32);
+            middle_pieces.insert(36);
+
+            middle_pieces
+        };
+    }
 
     let black_score: i64 = state
         .board
         .current_players_pieces(0)
         .iter()
-        .map(|&elt| if middle_pieces.contains(&elt) { 1 } else { 0 })
+        .map(|&elt| if MIDDLE_PIECES.contains(&elt) { 1 } else { 0 })
         .sum();
 
     let white_score: i64 = state
         .board
         .current_players_pieces(1)
         .iter()
-        .map(|&elt| if middle_pieces.contains(&elt) { 1 } else { 0 })
+        .map(|&elt| if MIDDLE_PIECES.contains(&elt) { 1 } else { 0 })
         .sum();
     unsigned100_normalize(-7, 7, black_score - white_score)
 }
